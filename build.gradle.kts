0c943406d7821d2e093e93370c0546265ba75fd5
Cristiano Fazi <98350514+Cristiano-Fazi@users.noreply.github.com>
2023-10-06 15:08:42 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,26 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0" apply false
+}
+
+application {
+    mainClass.set("org.jlleitschuh.gradle.ktlint.sample.kotlin.MainKt")
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+    filter {
+        exclude("**/ExampleInstrumentedTest.kt")
+    }
 }
