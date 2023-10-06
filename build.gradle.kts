778474bb361e2ba2e8affb735a6c077d90eb5444
Cristiano Fazi <98350514+Cristiano-Fazi@users.noreply.github.com>
2023-10-06 14:56:41 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0" apply false
 }
-
-// Configure the source directory you want to check
-ktlint {
-    source = files("app/src/main")
-}
