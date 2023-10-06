9f22a7e9f19398b08a50e51ce0e89cc0df211b4a
Cristiano Fazi <98350514+Cristiano-Fazi@users.noreply.github.com>
2023-10-06 13:18:37 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.6.0" apply false
+    //id("org.jlleitschuh.gradle.ktlint") version "11.6.0" apply false
 }
