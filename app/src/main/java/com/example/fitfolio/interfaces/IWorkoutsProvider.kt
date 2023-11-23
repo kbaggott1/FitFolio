ed62ec457238b4a053292e70f2c37c7e1ed7d6e3
kbaggott1 <93663166+kbaggott1@users.noreply.github.com>
2023-11-23 14:25:22 -0500

    90 create workouts repository interface (#103)
    * Add IWorkoutsProvider interface
    
    * Add workout data class

---
+++ app/src/main/java/com/example/fitfolio/interfaces/IWorkoutsProvider.kt
@@ -0,0 +1,11 @@
+package com.example.fitfolio.interfaces
+
+import com.example.fitfolio.data.Routine
+import com.example.fitfolio.data.Workout
+
+//Interface for routine providers
+interface IWorkoutsProvider {
+    fun getWorkouts(): List<Workout>
+    fun addWorkout(routine: Workout): Boolean
+    fun removeWorkout(routine: Workout): Boolean
+}
