048360261ae513630dad9440bbf90662a8fc2f8d
Cristiano Fazi <98350514+Cristiano-Fazi@users.noreply.github.com>
2023-11-24 14:48:04 -0500

    Added coroutines to read repository data (#108)
    * Added coroutines to read repository data
    
    added async
    
    * bug fix
    
    fix

--- app/src/main/java/com/example/fitfolio/viewmodels/RoutineViewModel.kt
+++ app/src/main/java/com/example/fitfolio/viewmodels/RoutineViewModel.kt
@@ -5,9 +5,16 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.example.fitfolio.data.Repository
 import com.example.fitfolio.data.Routine
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+
 class RoutineViewModel(private val repository: Repository) : ViewModel() {
     val routines: SnapshotStateList<Routine>
-        get() = repository.getRoutines().toMutableStateList()
+        get() = runBlocking {
+            async {
+                repository.getRoutines()
+            }.await().toMutableStateList()
+        }
 
     fun add(routine: Routine) {
         repository.addRoutine(routine)
