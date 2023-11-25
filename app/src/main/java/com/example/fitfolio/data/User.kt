d4acbf7cdaf91c5952137d20f48c5fc563dee5b4
Cristiano Fazi <98350514+Cristiano-Fazi@users.noreply.github.com>
2023-11-25 13:29:42 -0500

    Added Nessasary Gradle Code (#111)
    Added gradle code to use Firebase

--- app/src/main/java/com/example/fitfolio/data/User.kt
+++ app/src/main/java/com/example/fitfolio/data/User.kt
@@ -1,4 +1,4 @@
 package com.example.fitfolio.data
 
 //Class representing a single FitFolio user
-class User(var username: String, var password: String) { }
+class User(var id: String, var username: String, var password: String) { }
